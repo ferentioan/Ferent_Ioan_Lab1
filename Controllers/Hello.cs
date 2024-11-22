d8fff9f293252b2fedc792947fe611649ef996c2
Ioan Ferent <ferent.ionut@outlook.com>
2024-11-22 19:56:38 +0200

    Am modificat Index, SalutUtilizator si MesajPersonalizat in Hello Controller

---
+++ Controllers/Hello.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ferent_Ioan_Lab1.Controllers
+{
+    public class Hello : Controller
+    {
+        public string Index()
+        {
+            return "Bine ai venit în aplicația noastră!";
+        }
+
+        public string SalutUtilizator()
+        {
+            return "Salut, utilizatorule!";
+        }
+
+        public string MesajPersonalizat(string nume)
+        {
+            return $"Salut, {nume}! Sperăm că ai o zi minunată!";
+        }
+    }
+}
